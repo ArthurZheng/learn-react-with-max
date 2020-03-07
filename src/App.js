42d7cef4875a960b0f781a7bcf2b71fca57a669e
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-07 17:09:39 +1100

    first functional component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React from 'react';
 import './App.css';
 
-// class App extends Component {
-//   render(){
-//     return (
-//       <div className="App">
-//         <h1>Hi, I am a React App</h1>
-//       </div>
-
-//     )
-//   }
-// }
-
-
-// class App extends Component {
-//   render(){
-//     // return (
-//     //   <div className="App">
-//     //     <h1>Hi, I am a React App</h1>
-//     //   </div>
-
-//     // )
-//     React.createElement("div", null, "h1", "Hi, I\'m a React App!")
-    // React.createElement("div",{className: "App"}, React.createElement("h1", null, "Hi, I am another React App"))
-//   }
-// }
-
-
-
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
         <h1>Hi, I am a React App</h1>
+        <Person />
     </div>
   );
 }
