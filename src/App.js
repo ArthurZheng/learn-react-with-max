68fad146333d52fc9dd7ffa9efa7f2819f947810
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-07 17:23:10 +1100

    Added Aux, also shows props.children

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     <div className="App">
         <h1>Hi, I am a React App</h1>
         <Person name='jun' age='30'/>
+        <Person name='jiangli' age='28'>My hobby is: watching TV soap operas.</Person>
+        <Person name='Sophie' age='3'/>
     </div>
   );
 }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Aux from '../Aux/Aux';
+
 // const Person = () => {
 //     return (
 //         <p>I am a person!</p>
 //     )
 // }
 
-const Person = (props) => <p>I am a person! My name is {props.name} and I am {props.age} years old.</p>
+// const Person = ({name, age}) => <p>I am a person! My name is {name} and I am {age} years old.</p>
+const Person = ({name, age, children}) => (
+    <Aux>
+            <p>I am a person! My name is {name} and I am {age} years old.</p>
+            <h2>{children}</h2>
+    </Aux>
+)
 export default Person;
