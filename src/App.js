6622e73b8564a42285511fef716a5ad79d495cd4
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-07 17:30:42 +1100

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
         <h1>Hi, I am a React App</h1>
-        <Person name='jun' age='30'/>
-        <Person name='jiangli' age='28'>My hobby is: watching TV soap operas.</Person>
-        <Person name='Sophie' age='3'/>
+        <Person name='John' age='20'/>
+        <Person name='Rose' age='18'>My hobby is: watching TV soap operas.</Person>
+        <Person name='Gigi' age='3'/>
     </div>
   );
 }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -11,8 +11,9 @@ import Aux from '../Aux/Aux';
 // const Person = ({name, age}) => <p>I am a person! My name is {name} and I am {age} years old.</p>
 const Person = ({name, age, children}) => (
     <Aux>
-            <p>I am a person! My name is {name} and I am {age} years old.</p>
-            <h2>{children}</h2>
+        <p>I am a person! My name is {name} and I am {age} years old.</p>
+        <h2>{children}</h2>
+        <hr />
     </Aux>
 )
 export default Person;
