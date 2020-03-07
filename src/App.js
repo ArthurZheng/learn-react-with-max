7ed7185900f6a6a96f3a202e6b88d52103791bb6
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-07 17:32:12 +1100

    conert functional component to class based component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import Person from './components/Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <Person name='John' age='20'/>
-        <Person name='Rose' age='18'>My hobby is: watching TV soap operas.</Person>
-        <Person name='Gigi' age='3'/>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+      <h1>Hi, I am a React App</h1>
+      <Person name='John' age='20'/>
+      <Person name='Rose' age='18'>My hobby is: watching TV soap operas.</Person>
+      <Person name='Gigi' age='3'/>
+  </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,13 +2,6 @@ import React from 'react';
 
 import Aux from '../Aux/Aux';
 
-// const Person = () => {
-//     return (
-//         <p>I am a person!</p>
-//     )
-// }
-
-// const Person = ({name, age}) => <p>I am a person! My name is {name} and I am {age} years old.</p>
 const Person = ({name, age, children}) => (
     <Aux>
         <p>I am a person! My name is {name} and I am {age} years old.</p>
