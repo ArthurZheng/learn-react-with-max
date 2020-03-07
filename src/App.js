007c3761f03ced4a4f8fc1abd5a2b5a964955d41
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-08 10:16:00 +1100

    use useState, convert class based to functional component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 
 import Person from './components/Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Alex', age: 28, hobby: 'watching movie'},
-      {name: 'Bro', age: 12, hobby: 'gong fishing'},
-      {name: 'Maru', age: 89, hobby: 'cycling'},
-    ],
-    isSold: false,
-  }
-
-  switchNameHandler = () => {
-    console.log("Inside switchNameHandler, button was clicked");
-    this.setState({
+function App() {
+  const [personsState, setPersonsState] = useState(
+    {
       persons: [
-        {name: 'Alexima', age: 88, hobby: 'watching movie'},
-        {name: 'Brod', age: 27, hobby: 'gong fishing'},
-        {name: 'Maruooo', age: 99, hobby: 'cycling'},
-      ]
-    })
-  }
+        {name: 'Alex', age: 28, hobby: 'watching movie'},
+        {name: 'Bro', age: 12, hobby: 'gong fishing'},
+        {name: 'Maru', age: 89, hobby: 'cycling'},
+      ],
+      isSold: false,
+    }
+  )
 
-  render(){
-    return(
-      <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch name button</button>
-        {this.state.persons.map((person, index) => (
-          <Person key={index} name={person.name} age={person.age} >My hobby is: {person.hobby} </Person>
-        ))}
-      </div>
-    )
-  }
+  const switchNameHandler = () => {
+      console.log("Inside switchNameHandler, button was clicked");
+      setPersonsState({
+        persons: [
+          {name: 'Alexima', age: 88, hobby: 'watching movie'},
+          {name: 'Brod', age: 27, hobby: 'gong fishing'},
+          {name: 'Maruooo', age: 99, hobby: 'cycling'},
+        ]
+      })
+    }
+
+  return(
+    <div className="App">
+      <h1>Hi, I am a React App</h1>
+      <button onClick={switchNameHandler}>Switch name button</button>
+      {personsState.persons.map((person, index) => (
+        <Person key={index} name={person.name} age={person.age} >My hobby is: {person.hobby} </Person>
+      ))}
+    </div>
+  )
 }
 
 export default App;
