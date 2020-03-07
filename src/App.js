816850500c39081a633dc7723660348e6fb2ac5f
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-07 17:03:38 +1100

    Understadning JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// class App extends Component {
+//   render(){
+//     return (
+//       <div className="App">
+//         <h1>Hi, I am a React App</h1>
+//       </div>
+
+//     )
+//   }
+// }
+
+
+// class App extends Component {
+//   render(){
+//     // return (
+//     //   <div className="App">
+//     //     <h1>Hi, I am a React App</h1>
+//     //   </div>
+
+//     // )
+//     React.createElement("div", null, "h1", "Hi, I\'m a React App!")
+    // React.createElement("div",{className: "App"}, React.createElement("h1", null, "Hi, I am another React App"))
+//   }
+// }
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hi, I am a React App</h1>
     </div>
   );
 }
