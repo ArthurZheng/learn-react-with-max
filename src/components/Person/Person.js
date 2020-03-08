17bf93ddc127c6172a7a62b82be2f1394fe742d8
Jiangli SHI <jianglishi@192-168-1-103.tpgi.com.au>
2020-03-09 07:34:24 +1100

    inline style exaple, it only applies to the local component scope

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,9 +3,17 @@ import React from 'react';
 // import Aux from '../Aux/Aux';
 import './Person.css';
 
+const inLineStyle = {
+    backgroundColor: 'red',
+    font: 'inherit',
+    border: 'solid 2px blue',
+    padding: '15px',
+    cursor: 'pointer',
+}
+
 const Person = ({name, age, click, children, nameChanged}) => (
     <div className="Person">
-        <button onClick={click}>Click to Change</button>
+        <button  style={inLineStyle} onClick={click}>Click to Change</button>
         <p>I am a person! My name is {name} and I am {age} years old.</p>
         <h2>{children}</h2>
         <input type="text" onChange={nameChanged} value={name} />
